9dfe0a7e6b2b8e4c3db461b8e8dc964d593eed5f
Javier Cardoso <cardoso.javier@outlook.com>
2024-08-29 00:14:01 -0500

    added icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,16 +12,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-background">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="./favicon.ico" sizes="any" />
         <link
           rel="icon"
-          href="/icon?<generated>"
+          href="./icon?<generated>"
           type="image/<generated>"
           sizes="<generated>"
         />
         <link
           rel="apple-touch-icon"
-          href="/apple-icon?<generated>"
+          href="./apple-icon?<generated>"
           type="image/<generated>"
           sizes="<generated>"
         />

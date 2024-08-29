8c0169867c0ecc0ebc294c92ac448c7fa0d4f5b1
Javier Cardoso <cardoso.javier@outlook.com>
2024-08-29 12:04:46 -0500

    fixed project list headers

--- src/app/components/listItemHeader.js
+++ src/app/components/listItemHeader.js
@@ -2,17 +2,15 @@ import { ArrowUpRightCircle } from "react-bootstrap-icons";
 
 export default function ListItemHeader({ header, link, company = "" }) {
   return (
-    <div>
-      <a
-        href={link}
-        target="_blank"
-        className="flex flex-row gap-2 font-medium text-slate-200 leading-snug transition-all ease-in-out duration-500 hover:text-teal-400 hover:gap-5"
-      >
-        <span className="inline-flex items-baseline gap-2">
-          {header} {company}
-        </span>
-        <ArrowUpRightCircle />
-      </a>
-    </div>
+    <a
+      href={link}
+      target="_blank"
+      className="flex flex-row w-fit gap-2 font-medium text-slate-200 leading-snug transition-all ease-in-out duration-500 hover:text-teal-400 hover:gap-5"
+    >
+      <span className="inline-flex items-baseline gap-2">
+        {header} {company}
+      </span>
+      <ArrowUpRightCircle />
+    </a>
   );
 }
